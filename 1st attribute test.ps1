a055b41d4b59b09a3ce77a83b84aeb66672fc54d
kurtsmokealot <84984112+kurtchatelain@users.noreply.github.com>
2021-12-30 16:38:05 -0600

    Add files via upload
    This adds a proxy address of firstname.lastname@funimation.com to a test group in AD. Does not replace any existing proxy addresses.

---
+++ 1st attribute test.ps1
@@ -0,0 +1,26 @@
+﻿[string]$proxydomain = "@funimation.com"; #Proxy domain
+[string]$usersOU = "OU=Users,OU=Test-Attribute,DC=Corp,DC=Group1200,DC=com"; #OU to apply changes
+[int]$count = 0 ;
+
+Import-Module ActiveDirectory
+
+  Get-ADUser -Filter "*" -SearchScope Subtree -SearchBase "$usersOU" -Properties proxyAddresses, employeeType, givenName, Surname | foreach-object {
+
+  Write-Host "Editing user: $_.SamAccountName"
+
+    if ($_.Proxyaddresses -match $_.givenName+"."+$_.Surname+$proxydomain)
+    {
+      Write-Host "Result: PproxyAddresses value already exists; No action taken."
+    }
+    else
+    {
+      Set-ADUser -Identity $_.SamAccountName -Add @{proxyAddresses="SMTP:"+$_.givenName+"."+$_.Surname+$proxydomain}
+
+      Write-Host "Result: Added proxyAddresses value to Account"
+      $count++
+    }
+       
+
+  }
+
+Write-Host "Sucessfully Edited" $count "users"
